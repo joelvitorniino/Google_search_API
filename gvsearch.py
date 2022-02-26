81f93aa0b20ef691ec65a760b7b0205d12749b6b
joelvitorniino <joelprofessor13@gmail.com>
2022-02-26 01:03:35 -0300

    aaa

--- gvsearch.py
+++ gvsearch.py
@@ -1,7 +1,40 @@
-from app import create_app
+from flask import Flask, request, jsonify
+from googlesearch import search
 import os
 
-app = create_app()
+app = Flask(__name__)
 
-if os.environ.get("ENV") == "development" and __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+
+@app.route("/search_all", methods=["GET"])
+def get_all():
+    payload = request.args.get("q")
+
+    if payload == None:
+        return jsonify({"sucess": False, "error": "Q nao fornecido"})
+
+    lang = request.args.get("lang") if request.args.get("q") else "pt"
+
+    results = [c for c in search(f"{payload}", stop=30, lang=lang)]
+
+    return jsonify({"sucess": True, "links": results})
+
+
+@app.route("/search_video", methods=["GET"])
+def get_videos():
+    payload = request.args.get("q")
+
+    if payload == None:
+        return jsonify({"sucess": False, "error": "Q não fornecido"})
+
+    lang = request.args.get("lang") if request.args.get("q") else "pt"
+
+    results = [
+        c for c in search(f"'{payload}' youtube", stop=30, lang=lang) if "watch?v=" in c
+    ]
+
+    return jsonify({"sucess": True, "links": results})
+
+
+if os.environ.get("ENV") == "development":
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0")
